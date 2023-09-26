f0a0762dc61cc4e4abbd872b2914417686d77629
RonnieBk <veronicabrvk00@gmail.com>
2023-09-26 19:24:37 +0200

    task-02 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,22 +1,18 @@
-// const Player = require('@vimeo/player');
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
-// var _ = require('lodash.throttle');
+const player = new Vimeo(iframe);
 
-const onPlay = data => {
+const onPlay = throttle(data => {
   try {
     localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
-    console.log(localStorage.getItem('videoplayer-current-time'));
   } catch (error) {
     console.error('Get state error: ', error.message);
   }
-};
+}, 1000);
 
-player.on(
-  'timeupdate',
-  _.throttle(data => onPlay(data), 1000)
-);
+player.on('timeupdate', onPlay);
 
 const storagedTime = localStorage.getItem('videoplayer-current-time');
 const currentTime = JSON.parse(storagedTime);
