1ca95b862d989f0d4e4e2c1eb7f66f46ffe988dd
RonnieBk <veronicabrvk00@gmail.com>
2023-09-26 02:29:42 +0200

    Task 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+// const Player = require('@vimeo/player');
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+// var _ = require('lodash.throttle');
+
+const onPlay = data => {
+  try {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+    console.log(localStorage.getItem('videoplayer-current-time'));
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+
+player.on(
+  'timeupdate',
+  _.throttle(data => onPlay(data), 1000)
+);
+
+const storagedTime = localStorage.getItem('videoplayer-current-time');
+const currentTime = JSON.parse(storagedTime);
+const timeInSeconds = currentTime.seconds;
+player.setCurrentTime(timeInSeconds);
